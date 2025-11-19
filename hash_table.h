32bc0ad8a7c87b1f5670dac1f0b0d9e481147620
Rixda9 <78431473+Rixda9@users.noreply.github.com>
2025-11-20 00:06:45 +0100

    Add base setup

---
+++ hash_table.h
@@ -0,0 +1,7 @@
+typedef struct HashTable HashTable;
+
+HashTable* hash_table_create(size_t capacity);
+void hash_table_insert(HashTable *ht, const char *key, const char *value);
+char *hash_table_get(HashTable *ht, const char *key);
+void hash_table_delete(HashTable *ht, const char *key);
+void hash_table_destroy(HashTable *ht);
