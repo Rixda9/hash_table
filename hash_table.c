d3ed70704bb8a78ab2811494aeccd1c3bfcd26a3
Rixda9 <78431473+Rixda9@users.noreply.github.com>
2025-11-20 12:49:40 +0100

    Add create function with allocation error handling

--- hash_table.c
+++ hash_table.c
@@ -1,22 +1,51 @@
 #include "hash_table.h"
+#include <string.h>
+#include <stdlib.h>
 
-struct HashTable {
-  
-  int capacity;
+
+typedef struct HT_Items {
+
   char *key;
   char *value;
+  struct HT_Items *next;
+
+} HT_Items ;
+
+typedef struct HashTable {
+  
+  int capacity;
+  int size; 
+  HT_Items **items;
 
 } HashTable;
 
 
+
+
 HashTable* hash_table_create(size_t capacity)
 {
+  HashTable *ht = malloc(sizeof(HashTable));
+  if (ht == NULL) 
+  {
+    return  NULL;
+  }
+  ht->capacity = capacity;
+  ht->size = 0;
 
+  ht->items = calloc(capacity, sizeof(HT_Items *));
+  if (ht->items == NULL) 
+  {
+    free(ht);
+    return NULL;
+  }
+  return ht;
 }
 
 void hash_table_insert(HashTable *ht, const char *key, const char *value)
 {
+  for (int i = 0; i < strlen(key); i++) {
 
+  }
 }
 
 char *hash_table_get(HashTable *ht, const char *key)
--- hash_table.h
+++ hash_table.h
@@ -1,7 +1,15 @@
+#ifdef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <stddef.h>
+
 typedef struct HashTable HashTable;
+
 
 HashTable* hash_table_create(size_t capacity);
 void hash_table_insert(HashTable *ht, const char *key, const char *value);
 char *hash_table_get(HashTable *ht, const char *key);
 void hash_table_delete(HashTable *ht, const char *key);
 void hash_table_destroy(HashTable *ht);
+
+#endif
