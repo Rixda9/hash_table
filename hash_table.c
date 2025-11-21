5db120081575fc0922c5cee0f02ad5de93cd9d82
Rixda9 <78431473+Rixda9@users.noreply.github.com>
2025-11-21 12:37:25 +0100

    Add insert function

--- hash_table.c
+++ hash_table.c
@@ -22,41 +22,69 @@ typedef struct HashTable {
 
 
 
-HashTable* hash_table_create(size_t capacity)
-{
+HashTable* hash_table_create(size_t capacity) {
   HashTable *ht = malloc(sizeof(HashTable));
-  if (ht == NULL) 
-  {
+  if (ht == NULL) {
     return  NULL;
   }
   ht->capacity = capacity;
   ht->size = 0;
 
   ht->items = calloc(capacity, sizeof(HT_Items *));
-  if (ht->items == NULL) 
-  {
+  if (ht->items == NULL) {
     free(ht);
     return NULL;
   }
   return ht;
 }
 
-void hash_table_insert(HashTable *ht, const char *key, const char *value)
-{
-  for (int i = 0; i < strlen(key); i++) {
+void hash_table_insert(HashTable *ht, const char *key, const char *value) {
+  unsigned long hash_value = 5381;
 
+  // create a variable so strlen doesnt have to iterate everyloop
+  size_t len = strlen[key];
+  for (int i = 0; i < len; i++) {
+    hash_value = (hash_value << 5) + hash_value;
+    hash_value += key[i];
   }
+  int index = hash_value % ht->capacity;
+
+  // need to improve this part later {
+  HT_Items *new_item = malloc(sizeof(HT_Items);
+  if (new_item == NULL) {
+    return NULL;
+  }
+
+  new_item->key = malloc(strlen(key) + 1);
+  if (new_item->key == NULL) {
+    free(new_item); 
+    return NULL;
+  }
+  strcpy(new_item->key, key);
+
+  new_item->value = malloc(strlen(value) + 1);
+  if (new_item->value == NULL) {
+    free(new_item->key);
+    free(new_item);
+    return NULL;
+  }
+  strcpy(new_item->value, value);
+  
+  new_item->next = ht->items[index];
+
+  ht->items[index] = new_item;
+
+  ht->size++;
+  // }
 }
 
-char *hash_table_get(HashTable *ht, const char *key)
-{
+char *hash_table_get(HashTable *ht, const char *key) {
 
 }
-void hash_table_delete(HashTable *ht, const char *key){
+void hash_table_delete(HashTable *ht, const char *key) {
 
 }
 
-void hash_table_destroy(HashTable *ht)
-{
+void hash_table_destroy(HashTable *ht) {
 
 }
